3d94c99baedb39f91f2d878d4b01d11265ffa679
Vitali Hantarenka <vitalygontarenko@yandex.ru>
2019-03-24 23:50:53 +0300

    solve some tasks

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,120 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		const node = new Node(data, priority)
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (!this.isEmpty()) {
+			let removedRoot = this.detachRoot();
+			this.restoreRootFromLastInsertedNode(removedRoot);
+			this.shiftNodeDown(this.root);
+			return removedRoot.data;
+		}
 	}
 
 	detachRoot() {
-		
+		let lastRoot = this.root;
+		this.parentNodes = this.parentNodes.filter((node)=> node != lastRoot);
+		this.root = null;
+		return lastRoot;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		if (detached.left) {
+            this.root = this.parentNodes[this.parentNodes.length - 1]
+
+            if (this.parentNodes.length <= 2) {
+				this.parentNodes.unshift(this.root)
+			} else if (this.root.parent.right == this.root && this.root.parent != detached) {
+				this.parentNodes.unshift(this.root.parent);
+			}
+			
+            this.parentNodes.splice(this.parentNodes.lastIndexOf(this.root), 1);
+
+            if (this.root.parent.left == this.root) {
+                this.root.parent.left = null;
+            } else {
+				this.root.parent.right = null;
+			}
+
+			this.root.left = detached.left;
+			
+            if (this.root.left) {
+				this.root.left.parent = this.root;
+			}
+
+			this.root.right = detached.right;
+			
+            if (this.root.right) {
+				this.root.right.parent = this.root;
+			}
+
+            this.root.parent = null;
+		}
 	}
 
 	size() {
-		
+		return this.parentNodes.length + 1;
 	}
 
 	isEmpty() {
-		
+		if (!this.root){
+			return true;
+		}
+
+		return false;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (!this.root) {
+			this.root = node;
+			this.parentNodes.push(node);
+		} else {
+			node.parent = this.parentNodes[0];
+			if (!node.parent.left) {
+				node.parent.left = node;
+			} else if (!node.parent.right) {
+				node.parent.right = node;
+				this.parentNodes.shift();
+			}
+			this.parentNodes.push(node);
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+		if (!node.parent){
+			this.root = node;
+		}
+
+		if (node.parent && node.parent.priority < node.priority) {
+
+			if (this.parentNodes.indexOf(node.parent) == -1){
+				this.parentNodes.splice(this.parentNodes.indexOf(node), 1, node.parent);
+				this.root = node;
+			} else {
+				this.parentNodes[this.parentNodes.indexOf(node)] = node.parent;
+				this.parentNodes[this.parentNodes.indexOf(node.parent)] = node;
+			}
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		}
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,22 +1,82 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+		node.parent = this;
+		if(this.left && this.right) {
 
+		} else if (!this.left) {
+			this.left = node;
+		} else if(this.left) {
+			this.right = node;
+		}
 	}
 
 	removeChild(node) {
-
+		if (node === this.left){
+			this.left.parent = null;
+			this.left = null;
+		} else if (node === this.right) {
+			this.left.parent = null;
+			this.right = null;
+		} else if (node !== this.left && node !== this.right) {
+			throw "error! node is not a child of this node"
+		}
 	}
 
 	remove() {
-
+		if (this.parent != null) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+
+			let rightChildThis = this.right;
+			let leftChildThis = this.left;
+			let parent = this.parent;
+			let grandParent = this.parent.parent;
+
+			if (this.parent.right === this) {
+
+				if (this.parent.left) {
+					this.parent.left.parent = this;
+				}
+
+				this.right = parent;
+				this.left = parent.left;
+			} else if (this.parent.left === this) {
+
+				if (this.parent.right) {
+					this.parent.right.parent = this;
+				}
+
+				this.left = parent;
+				this.right = parent.right;
+			}
+
+			this.parent = grandParent;
+			if (grandParent) {
+				if (grandParent.left === parent) {
+					grandParent.left = this;
+				} else if (grandParent.right === parent) {
+					grandParent.right = this;
+				}
+			}
+
+
+			parent.parent = this;
+			parent.right = rightChildThis;
+			parent.left = leftChildThis;
+		}
+
 	}
 }
 
